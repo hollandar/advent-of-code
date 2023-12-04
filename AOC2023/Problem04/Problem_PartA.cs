99ba221237abd485c90b905ebcb6dc3c4a4c0cef
Adrian Holland <hollandar@gmail.com>
2023-12-04 17:19:29 +1100

    Scratchcards  2023

--- AOC2023/Problem04/Problem_PartA.cs
+++ AOC2023/Problem04/Problem_PartA.cs
@@ -1,15 +1,32 @@
 ﻿using AOCLib;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023.Problem04;
 
 public partial class Problem : ProblemPart<InputRow>
 {
+    [GeneratedRegex("(?<Value>\\d+|\\|)")]
+    protected partial Regex NumberRegex();
+
     protected override string PartA(IEnumerable<InputRow> datas)
     {
         int answer = 0;
         foreach (var data in datas)
         {
-            
+            var matches = NumberRegex().Matches(data.Value);
+            var matchValues = matches.Select(r => r.Value);
+            var cardNumber = int.Parse(matchValues.First());
+            var winningNumbers = matchValues.Skip(1).TakeWhile(r => r != "|").Select(r => int.Parse(r)).ToHashSet();
+            var myNumbers = matchValues.SkipWhile(r => r != "|").Skip(1).Select(r => int.Parse(r)).ToHashSet();
+
+            var winningCount = winningNumbers.Intersect(myNumbers).Count();
+            var value = winningCount > 0 ? 1: 0;
+            for (int i = 1; i < winningCount; i++)
+            {
+                value = value * 2;
+            }
+
+            answer += value;
         }
 
         return answer.ToString();
--- AOC2023/Problem04/Problem_PartB.cs
+++ AOC2023/Problem04/Problem_PartB.cs
@@ -4,14 +4,64 @@ namespace AdventOfCode2023.Problem04;
 
 public partial class Problem : ProblemPart<InputRow>
 {
+    class Card
+    {
+        public int CardNumber { get; set; }
+        public HashSet<int> WinningNumbers { get; set; }
+        public HashSet<int> MyNumbers { get; set; }
+        public int WinningCount { get; set; }
+        public int Copies { get; internal set; }
+    }
+
     protected override string PartB(IEnumerable<InputRow> datas)
     {
-        long answer = 0;
+        int answer = 0;
+        var cards = new Dictionary<int, Card>();
+
         foreach (var data in datas)
         {
-            
+            var matches = NumberRegex().Matches(data.Value);
+            var matchValues = matches.Select(r => r.Value);
+            var cardNumber = int.Parse(matchValues.First());
+            var winningNumbers = matchValues.Skip(1).TakeWhile(r => r != "|").Select(r => int.Parse(r)).ToHashSet();
+            var myNumbers = matchValues.SkipWhile(r => r != "|").Skip(1).Select(r => int.Parse(r)).ToHashSet();
+
+            var winningCount = winningNumbers.Intersect(myNumbers).Count();
+            var value = winningCount > 0 ? 1 : 0;
+            for (int i = 1; i < winningCount; i++)
+            {
+                value = value * 2;
+            }
+
+            cards[cardNumber] = new Card
+            {
+                CardNumber = cardNumber,
+                WinningNumbers = winningNumbers,
+                MyNumbers = myNumbers,
+                WinningCount = winningCount,
+                Copies = 0
+            };  
         }
 
+        var currentCard = 1;
+        while (currentCard <= cards.Keys.Max())
+        {
+            var card = cards[currentCard];
+
+            for (int j = 0; j <= card.Copies; j++) // <= because we need to count the current card
+            {
+                for (int i = 0; i < card.WinningCount; i++)
+                {
+                    cards[currentCard + i + 1].Copies++;
+                }
+            }
+
+            answer += (card.Copies + 1);
+            currentCard++;
+        }
+
+        
+        
         return answer.ToString();
     }
 
